da784cff1bfd5e15119dcaa6da21ae6a9ce2a91b
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2022-01-17 02:31:40 +0600

    implement frequently asked and subscribe section

--- js/index.js
+++ js/index.js
@@ -3,8 +3,21 @@ document.getElementById("collapse").addEventListener("click", function () {
   document.getElementById("frofileContainer").classList.toggle("active");
 });
 
-document.getElementById("close_icon").addEventListener("click", function () {
-  document.getElementById("navbarContainer").style.display = "none";
-  document.getElementById("frofileContainer").style.display = "none";
-  document.getElementById("close_icon").style.display = "none";
-});
+document
+  .getElementById("collapse_question")
+  .addEventListener("click", function () {
+    document.getElementById("hide_info").classList.toggle("col");
+    document.getElementById("collapse_question").classList.toggle("minus");
+  });
+document
+  .getElementById("collapse_questionTow")
+  .addEventListener("click", function () {
+    document.getElementById("hide_infoTow").classList.toggle("col");
+    document.getElementById("collapse_question").classList.toggle("minus");
+  });
+document
+  .getElementById("collapse_questionThree")
+  .addEventListener("click", function () {
+    document.getElementById("hide_infoThree").classList.toggle("col");
+    document.getElementById("collapse_question").classList.toggle("minus");
+  });
